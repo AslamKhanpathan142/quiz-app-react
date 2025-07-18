fa7547fd1948afcab6a5551a30e5aaf6342e60b9
aslamkhanpatha142 <khanaslampathan142@gmail.com>
2025-07-18 09:14:32 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        question : "What is React?",
+        opt1 : "A JavaScript library for building user interfaces",
+        opt2 : "A server-side framework",
+        opt3 : "A programming language",
+        opt4 : "A database management system",
+        ans : 1,
+    },
+    {
+        question : "How do you create a React component?",
+        opt1 : "By using the function keyword or class keyword",
+        opt2 : "By using the new keyword",
+        opt3 : "By using the component keyword",
+        opt4 : "By using the extends keyword only",
+        ans : 1,
+    },
+    {
+        question : "What is JSX?",
+        opt1 : "A JavaScript file",
+        opt2 : "A CSS preprocessor",
+        opt3 : "A syntax extension for JavaScript",
+        opt4 : " A database query language",
+        ans : 3,
+    },
+    {
+        question : " Which method in a React class component is used to handle side effects, such as fetching data?",
+        opt1 : "`render()`",
+        opt2 : "`componentDidMount()`",
+        opt3 : "`constructor()`",
+        opt4 : "`componentWillUnmount()`",
+        ans : 2,
+    },
+    {
+        question : "How do you pass data from a parent component to a child component?",
+        opt1 : "Using state",
+        opt2 : "Using props",
+        opt3 : "Using setState",
+        opt4 : "Using render",
+        ans : 2,
+    },
+    
+]
